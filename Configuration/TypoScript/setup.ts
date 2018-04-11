a9840911b08f3fb0fa61290dc2ca2655cc4a8d21
Tanel Põld <tanel@brightside.ee>
2018-04-11 13:55:44 +0800

    Make it possible to load static template before fluid_styled_content

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -2,7 +2,7 @@ page.includeCSS.youtubevideo = {$youtubevideo.cssPath}
 page.includeJSFooterlibs.youtubevideo = {$youtubevideo.javascriptPath}
 
 tt_content {
-	youtubevideo_pi1 < lib.fluidContent
+	youtubevideo_pi1 =< lib.fluidContent
 	youtubevideo_pi1 {
 	  templateName = Youtubevideo
 	  templateRootPaths.200 = {$youtubevideo.templateRootPaths}
@@ -17,7 +17,7 @@ plugin.tx_youtubevideo {
 		# language code use de, fr, et etc.
 		hl = TEXT
 		hl.value = {$youtubevideo.hl}
-		
+
     # wide screen 16:9 value (math behind: 9 * 100 / 16 = 56.25)
     ratioDefault = TEXT
     ratioDefault.value = 56.25
@@ -31,11 +31,11 @@ plugin.tx_youtubevideo {
     modestbranding = TEXT
     modestbranding.value = {$youtubevideo.modestbranding}
     # If set to '1' disables the YouTube logo on video player toolbar.
-    
+
     # custom cover image width (if set 640 or smaller and no custom image set sddefault image from youtube will be used instead of maxresdefault)
     coverimagewidth = TEXT
     coverimagewidth.value = {$youtubevideo.coverimagewidth}
-    
+
 		# if set to 0 and cover text is not present fetching cover image from youtube will be turned off
     enablecovertoall = TEXT
     enablecovertoall.value = {$youtubevideo.enablecovertoall}
