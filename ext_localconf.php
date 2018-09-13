5bf06c78f4e7421cffeaabcebb102c0d7827339b
Tanel Põld <tanel@brightside.ee>
2018-09-13 05:29:29 +0300

    Cleanup

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,10 +1,7 @@
 <?php
 defined('TYPO3_MODE') || die ('Access denied.');
 
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:youtubevideo/Configuration/PageTS/setup.ts">');
-
-
 
 $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
 $iconRegistry->registerIcon(
@@ -14,6 +11,3 @@ $iconRegistry->registerIcon(
 );
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['youtubevideo_pi1'] = \Brightside\Youtubevideo\Hooks\PageLayoutView\YoutubevideoContentElementPreviewRenderer::class;
-   
-//   $TYPO3_CONF_VARS['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem'][] = 
-//   'Brightside\\Youtubevideo\\Hooks\\PageLayoutView\\YoutubevideoContentElementPreviewRenderer';
