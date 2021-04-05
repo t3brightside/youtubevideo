cc36379fd12d9cd62fffd9aaecb94182077660e4
Tanel Põld <tanel@brightside.ee>
2021-04-05 13:55:59 -0400

    Ver change

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 		'title' => 'YouTube Video',
 		'description' => 'Easy to use YouTube video content element with responsive template, cover image and backend preview.',
 		'category' => 'fe',
-		'version' => '1.4.2',
+		'version' => '1.4.3',
 		'state' => 'stable',
 		'uploadfolder' => false,
 		'createDirs' => '',
