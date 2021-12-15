8768f16cb7101b1608f6bc14af9437ec74a81bc1
Tanel Põld <tanel@brightside.ee>
2021-12-15 12:07:20 +0200

    [FEATURE] TYPO3 10.4 LTS compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
 	'author_company' => 'Brightside OÜ / t3brightside.com',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '11.5.0-11.5.99',
+			'typo3' => '10.4.0-11.5.99',
 			'fluid_styled_content' => '',
 		],
     ],
