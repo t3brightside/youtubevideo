51e2a9c645dd83fae9141fbf659ce572ba14eaf5
Tanel Põld <tanel@brightside.ee>
2023-03-20 14:15:20 +0200

    [TASK] Clean up

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,23 +1,26 @@
 <?php
-defined('TYPO3_MODE') || defined('TYPO3') || die('Access denied.');
-
 use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Imaging\IconRegistry;
 
-$versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
-// Only include page.tsconfig if TYPO3 version is below 12 so that it is not imported twice.
-if ($versionInformation->getMajorVersion() < 12) {
-   ExtensionManagementUtility::addPageTSConfig('
-      @import "EXT:youtubevideo/Configuration/page.tsconfig"
-   ');
-}
+defined('TYPO3') || die('Access denied.');
 
-$iconRegistry = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-$iconRegistry->registerIcon(
-  'youtubevideo_icon',
-  \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-  ['source' => 'EXT:youtubevideo/Resources/Public/Icons/ext_icon_content.svg']
-);
+(function () {
+    $versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
+    // Only include page.tsconfig if TYPO3 version is below 12 so that it is not imported twice.
+    if ($versionInformation->getMajorVersion() < 12) {
+        ExtensionManagementUtility::addPageTSConfig('
+            @import "EXT:youtubevideo/Configuration/page.tsconfig"
+        ');
+    }
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['Brightside\\Youtubevideo\\Evaluation\\HoursMinutesSeconds'] = '';
+    $iconRegistry = GeneralUtility::makeInstance(IconRegistry::class);
+    $iconRegistry->registerIcon(
+        'youtubevideo_icon',
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+    ['source' => 'EXT:youtubevideo/Resources/Public/Icons/ext_icon_content.svg']
+    );
+
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['Brightside\\Youtubevideo\\Evaluation\\HoursMinutesSeconds'] = '';
+})();
