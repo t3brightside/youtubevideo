a54ffca37d33f55861f08f90077020762f00098a
Tanel Põld <tanel@brightside.ee>
2023-04-27 08:36:57 +0300

    [RELEASE] 2.4.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = [
 	'title' => 'YouTube Video',
 	'description' => 'YouTube videos with custom cover images, gallery layout, pagination, GDPR options and backend previews.',
 	'category' => 'fe',
-	'version' => '2.3.0',
+	'version' => '2.4.0',
 	'state' => 'stable',
 	'clearcacheonload' => true,
 	'author' => 'Tanel Põld',
@@ -12,8 +12,8 @@ $EM_CONF[$_EXTKEY] = [
 	'author_company' => 'Brightside OÜ / t3brightside.com',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '11.5.0-12.99.99',
-			'fluid_styled_content' => '',
+			'typo3' => '11.5.0-12.4.99',
+			'fluid_styled_content' => '11.5.0-12.4.99',
             'embedassets' => '1.2.0-1.99.99',
 		],
     ],
