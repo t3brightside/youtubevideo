dd362e34cfcf733cb983eab954ff59ec03cfa1ab
Tanel Põld <tanel@brightside.ee>
2023-12-08 09:45:06 +0200

    [TASK] Add dependency on paginatedprocessors

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,6 +15,7 @@ $EM_CONF[$_EXTKEY] = [
 			'typo3' => '11.5.0-12.4.99',
 			'fluid_styled_content' => '11.5.0-12.4.99',
             'embedassets' => '1.2.0-1.99.99',
+            'paginatedprocessors' => '1.5.0-1.5.99'
 		],
     ],
 ];
